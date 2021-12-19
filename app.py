f82d2363e1bb4f9a7d60e65c5be6e337d5fcf0a3
Hannah Bacon <hannahbacon@hotmail.co.uk>
2021-12-19 14:03:07 +0000

    Add functionality for handling post request, to save uploaded file for further processing

--- app.py
+++ app.py
@@ -1,8 +1,42 @@
-from flask import render_template, Flask
+from flask import render_template, Flask, request, flash, redirect
+from werkzeug.utils import secure_filename
+import os
 
-app = Flask(__name__)
+
+UPLOAD_FOLDER = 'static/uploads/'
+DOWNLOAD_FOLDER = 'static/downloads/'
+ALLOWED_EXTENSIONS = {'wav'}
+app = Flask(__name__, static_url_path="/static")
 
 
-@app.route('/')
-def hello(name=None):
+# APP CONFIGURATIONS
+app.config['SECRET_KEY'] = 'mgc'  
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
+# limit upload size upto 6mb
+# app.config['MAX_CONTENT_LENGTH'] = 6 * 1024 * 1024
+
+
+
+def allowed_file(filename):
+    print('checking if valid file')
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index(name=None):
+    if request.method == 'POST':
+        print('Posting!!!!!!!!!!!')
+        if 'file' not in request.files:
+            flash('No file attached in request')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No file selected')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            print('uploading file...')
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
+        return render_template("index.html", data=name)
     return render_template('index.html', name=name)
